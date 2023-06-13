264406c242af3062c3b23a5e5777a1d8bbae1a7b
webinartelran <webinartelran@gmail.com>
2023-06-13 20:32:45 +0300

    CW6

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -9,16 +9,16 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class RegistrationTests {
+public class RegistrationTests extends TestBase{
 
-    WebDriver wd;
-
-    @BeforeMethod
-    public void init(){
-        wd = new ChromeDriver();
-        wd.navigate().to("https://telranedu.web.app/home");
-        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
+//    WebDriver wd;
+//
+//    @BeforeMethod
+//    public void init(){
+//        wd = new ChromeDriver();
+//        wd.navigate().to("https://telranedu.web.app/home");
+//        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//    }
 
     @Test
     public void registrationPositive(){
@@ -45,9 +45,20 @@ public class RegistrationTests {
         // Assert
 
 //        Assert.assertTrue(wd.findElements(By.xpath("//*[.='Sign Out']")).size() > 0);
-        Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
-
+//        Assert.assertTrue(wd.findElements(By.xpath("//button")).size() > 0);
+        pause(5000);
+        Assert.assertTrue(isElementPresent(By.xpath("//button")));
     }
+
+    @Test
+    public void registrationNegativeWrongEmail(){
+        int i = (int)(System.currentTimeMillis()/1000)%3600;
+        String email = "abc" + i + "def.com", password = "$Abcdef12345";
+        openLoginForm();
+        fillLoginForm(email, password);
+        submitRegistration();
+    }
+
 
     @AfterMethod
     public void tearDown(){
